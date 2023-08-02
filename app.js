782ac05b91229e6b136921db870bcfb9f4f93872
olajeks <olumuyiwajames@gmail.com>
2023-08-02 22:36:51 +0100

    Modified app.js
    commented out methods in cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
 const connectDb = require('./db/connect');
 const notFound = require('./middlewares/not-found');
